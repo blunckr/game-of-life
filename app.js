5f7e3352dbf5051a68ce76579e68c008584b2158
Ryker Blunck <rykerblunck@gmail.com>
2017-01-14 22:49:38 -0700

    added some patterns by hand, decided I don't want to do that anymore

--- app.js
+++ app.js
@@ -65,13 +65,15 @@
   }
 
   var presets={
-    Blinker: [[-1, 0], [0, 0], [1, 0]]
+    'R-pentomino':[[0,0], [0,-1], [1,-1], [-1,0], [0,1]],
+    'Acorn':[[1,0], [2,0], [3,0], [0,-1], [1,0], [-2,-2], [-2,0], [-3,0]],
+    'Diehard':[[-3,0], [-2,0], [-2,1], [2,1], [3,1], [4,1], [3,-1]],
   };
 
   function main(){
     var cellSize=5;
     var playing=false;
-    var currentPreset=presets.Blinker;
+    var currentPreset=presets['Diehard'];
     // currentPreset=null;
 
     var canvas=document.getElementById('game');
